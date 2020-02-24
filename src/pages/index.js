4668738b0ab94402f843b93524786e6a9aaeac62
Connor MacDonald <connor.macdonald@fmcat.net>
2020-02-23 20:07:45 -0500

    Updated information about lodging

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -13,7 +12,8 @@ const IndexPage = () => (
     <p>Room blocks are reserved at the <a href="https://www.mountainviewgrand.com/" target="_blank">Mountain View Grand</a>. To reserve, call the reservations department at (866) 801-3653 and mention the event code BH00009222 or our names.</p>
     <p>Room blocks are limited, so other options include:</p>
     <ul>
-      <li><a href="https://arlingtonhotelnh.com/" target="_blank">Arlington Hotel</a>, Bethlehem, NH</li>
+      <li><a href="https://www.cabotinnandsuites.com/" target="_blank">Cabot Inn and Suites</a>, Lancaster, NH (6.7 miles away)</li>
+      <li><a href="https://arlingtonhotelnh.com/" target="_blank">Arlington Hotel</a>, Bethlehem, NH (11.5 miles away)</li>
       <li><a href="https://www.airbnb.com/s/Northfield--NH--United-States/homes?refinement_paths%5B%5D=%2Fhomes&current_tab_id=home_tab&selected_tab_id=home_tab&source=mc_search_bar&search_type=unknown&screen_size=large&hide_dates_and_guests_filters=false&checkin=2020-10-16&checkout=2020-10-18&adults=2&ne_lat=43.80474850853288&ne_lng=-71.33910603027346&sw_lat=43.14102506926267&sw_lng=-71.81838459472658&zoom=10&search_by_map=true" target="_blank">Local AirBnBs</a> - We have had a lot of luck with Burns Pond options, but there are many nice options</li>
     </ul>
     <h2>RSVP</h2>
